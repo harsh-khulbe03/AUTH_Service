29e96b40b959bb7b24f251a8e220e246bd2dbcb7
Harsh Khulbe <harshkhulbe18@gmail.com>
2023-07-27 05:24:56 +0530

    Added token generation logic from sign in

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -17,6 +17,25 @@ class UserService {
     }
   }
 
+  async signIn(email, plainPassword) {
+    try {
+      // step 1: fetch the user using the email.
+      const user = await this.userRepository.getByEmail(email);
+      //step 2: compare incoming plain password with stored encrypted password.
+      const passwordMatch = this.checkPassword(plainPassword, user.password);
+      if (!passwordMatch) {
+        console.log("Password doesn't match");
+        throw { error: "Incorrect password" };
+      }
+      //step 3: if passwords match then create a token and send it to the user
+      const newJWT = this.createToken({ email: user.email, id: user.id });
+      return newJWT;
+    } catch (error) {
+      console.log("Something went wrong in the sign in process");
+      throw error;
+    }
+  }
+
   createToken(user) {
     try {
       const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
