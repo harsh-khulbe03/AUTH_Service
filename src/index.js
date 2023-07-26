bc4f9cdeb44583b9503bb235ce6c255eacca6a4a
Harsh Khulbe <harshkhulbe18@gmail.com>
2023-07-27 04:15:48 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -5,23 +5,20 @@ const { PORT } = require("./config/serverConfig");
 
 const apiRoutes = require("./routes/index");
 
-// const bcrypt = require("bcrypt");
-// const { User } = require("./models/index");
-// const UserRepository = require("./repository/user-repository");
-
+// const UserService = require("./services/user-service");
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/api", apiRoutes);
   app.listen(PORT, async () => {
     console.log(`Server started on port: ${PORT}`);
-    // const incomingpassword = "1234569";
-    // const user = await User.findByPk(3);
-    // const response = bcrypt.compareSync(incomingpassword, user.password);
-    // console.log(response);
-    // const repo = new UserRepository();
-    // const response = await repo.getById(1);
-    // console.log(response);
+    // const service = new UserService();
+    // const newToken = service.createToken({ email: "harsh@admin.com", id: 1 });
+    // console.log("new token is: ", newToken);
+    //   const token =
+    //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhhcnNoQGFkbWluLmNvbSIsImlkIjoxLCJpYXQiOjE2OTA0MTExNzMsImV4cCI6MTY5MDQxMTIwM30.dv6Bk5Th5TDPlsLaozDdSXgscyqmu75RW1bWmUAeQ_k";
+    //   const response = service.verifyToken(token);
+    //   console.log(response);
   });
 };
 
